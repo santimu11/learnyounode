1578cd8c66d8a6e06c894396feacbb91aefead65
Santi <santimu11@yahoo.es>
2016-10-03 17:36:33 +0200

    File server

---
+++ fileServer.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/plain'
+	});
+	var fichero = fs.createReadStream(process.argv[3]);
+	fichero.pipe(res);
+});
+
+server.listen(process.argv[2]);
