96fc327665fb512b5b3d3d9000de62386891307e
Santi <santimu11@yahoo.es>
2016-10-03 10:54:44 +0200

    Filtrar por extension modificado

--- filteredls.js
+++ filteredls.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 fs.readdir(process.argv[2], function callback(error, files) {
+	if (error) console.error(error);
 	files.forEach(function(file) {
 		if (path.extname(file) == ("." + process.argv[3])) {
 			console.log(file);
