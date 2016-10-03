ac67c82fc67a5b52d39072c448ac59f03e13e6d3
Santi <santimu11@yahoo.es>
2016-10-03 15:45:29 +0200

    Time Server

---
+++ timeServer.js
@@ -0,0 +1,8 @@
+var net = require('net');
+var strftime = require('strftime');
+
+var server = net.createServer(function(socket) {
+	socket.end(strftime('%F %H:%M'+'\n'));
+});
+server.listen(process.argv[2]);
+console.log('Escuchando en el puerto ' + process.argv[2]);
